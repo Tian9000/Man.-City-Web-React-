d533e9fde0514f2bf716090e30ca681c1b6a4913
Tian9000 <wijayacristian220@gmail.com>
2019-10-08 05:41:08 +0700

    Make Authentication File

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,21 @@
-import firebase from 'firebase/app';
-import 'firebase/app';
-import 'firebase/database';
+import firebase from 'firebase/app'
+import 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCMaFqKID_76BD7jIdP188umPdrF5gdSfo",
-    authDomain: "man-city-web.firebaseapp.com",
-    databaseURL: "https://man-city-web.firebaseio.com",
-    projectId: "man-city-web",
-    storageBucket: "",
-    messagingSenderId: "876253697425",
-    appId: "1:876253697425:web:1699708dee1a142c67f630",
-    measurementId: "G-2J37GKFK1V"
-  };
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 
-  const firebaseDB = firebase.database();
-  const firebaseMatches = firebaseDB.ref('matches');
-  const firebasePromotions = firebaseDB.ref('promotions');
+const firebaseDB = firebase.database();
+const firebaseMatches = firebaseDB.ref('matches');
+const firebasePromotions = firebaseDB.ref('promotions');
+const firebaseTeams = firebaseDB.ref('teams')
 
 export {
     firebase,
     firebaseMatches,
     firebasePromotions,
+    firebaseTeams,
+    firebaseDB
 }
